5d90dc75b8099260ce3790b7fb4708f838a932b8
Stephen Donner <stephen.donner@gmail.com>
2019-04-05 23:32:16 -0700

    bgcolor=red isn't in the initial bug comment; correcting

--- scripts/redbg.sh
+++ scripts/redbg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# The following "test" is directly from https://bugzilla.mozilla.org/show_bug.cgi?id=1511350#c0:
+# The following "test" is directly from https://bugzilla.mozilla.org/show_bug.cgi?id=1511350:
 
 adb shell am start -n org.mozilla.firefox/.App -a android.intent.action.VIEW -d "'data:text/html;base64,PGJvZHkgc3R5bGU9ImJhY2tncm91bmQtY29sb3I6cmVkIj4='"
